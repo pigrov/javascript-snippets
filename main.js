a628061fa1c505579f3495692f6e6c36b13996c9
Dmitry Pigrov <40947983+pigrov@users.noreply.github.com>
2024-02-08 10:03:23 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ element
     })
     .on(`paste`, function (e) {
         e.preventDefault();
+        // When copying, the text inserts the formatted text into the edited div.
         document.execCommand(
             `insertHTML`,
             false,
