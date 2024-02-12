4b33d5e86a5d0e4f57d507e67fc8ab751462e782
Dmitry Pigrov <40947983+pigrov@users.noreply.github.com>
2024-02-12 16:14:04 +0300

    Create debounce.js

---
+++ debounce.js
@@ -0,0 +1,14 @@
+function debounce(func, timeout = 300) {
+	let timer;
+	return (...args) => {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			func.apply(this, args);
+		}, timeout);
+	};
+}
+// function saveInput() {
+// 	console.log('Saving data');
+// }
+// const processChange = debounce(() => saveInput());
+// processChange();
