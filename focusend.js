72ca90cfdb20e372c9d25cca486911ac58010bc9
Dmitry Pigrov <40947983+pigrov@users.noreply.github.com>
2024-04-05 12:04:39 +0300

    Update focusend.js

--- focusend.js
+++ focusend.js
@@ -1,3 +1,12 @@
+// This JavaScript snippet defines a function called `focusend` that takes an `element` as a parameter. Inside the function, there is a `setTimeout` function with a delay of 0 milliseconds, which means it will be executed asynchronously as soon as possible. 
+
+// Within the `setTimeout` function, the following actions are performed:
+// 1. `element.focus()` - This focuses on the specified element.
+// 2. `window.getSelection().selectAllChildren(element)` - This selects all the children of the specified element.
+// 3. `window.getSelection().collapseToEnd()` - This collapses the selection to the end of the range.
+
+// Overall, this snippet is used to focus on an element, select all its children, and collapse the selection to the end. The use of `setTimeout` with a delay of 0 milliseconds allows these actions to be performed asynchronously.
+
 function focusend(element) {
 	setTimeout(() => {
 		element.focus();
